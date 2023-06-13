80157aa2f3c73dd6d5a67a7992501e1df5aeec55
herberth_lucho <herberth_lucho@unicomer.com>
2023-06-13 10:07:15 -0600

    upgrade material to v15

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,50 +1,31 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSelectModule } from '@angular/material/select';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { CdkStepperModule } from '@angular/cdk/stepper';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatInputModule,
+    MatIconModule,
     MatToolbarModule,
     MatSidenavModule,
     MatDividerModule,
-    MatSelectModule,
     MatStepperModule,
     MatDatepickerModule,
     MatNativeDateModule,
     CdkStepperModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatCardModule,
+    MatIconModule,
     MatExpansionModule,
-    MatTableModule,
-    MatCheckboxModule,
-    MatMenuModule,
-    MatTabsModule,
-    MatRadioModule,
-    MatSlideToggleModule,
+    MatIconModule,
   ],
 })
 export class MaterialModule {}
